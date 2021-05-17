e8c44afa03775c1d4dc091fc2159758d9217fa5f
ayman <aymanjaber2012@gmail.com>
2021-05-17 22:38:43 +0300

    Add build folder

---
+++ build/precache-manifest.13ce4a952d0c05ae313b23ddbff59a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb018597b9ae02bd3ecf4e872edd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eb1ddad1796f898867f",
+    "url": "/static/css/main.4a1c4ed1.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb1ddad1796f898867f",
+    "url": "/static/js/main.89eb4800.chunk.js"
+  },
+  {
+    "revision": "329102c932a6288a601d",
+    "url": "/static/js/runtime-main.c0c6e36b.js"
+  }
+]);
